7844f75ed052f88697fc75c4aff5cd7d0cc1ceb1
cedric smith <cedric012102@gmail.com>
2020-06-02 14:07:45 -0700

    added number

---
+++ index.js
@@ -0,0 +1,25 @@
+var priceOfShirt = 9.99;
+var priceOfPants = 40.00;
+
+var moneyInWallet = 5.00;
+var moneyOnCreditCard = 100.00;
+
+var numberOfFriends = 1;
+var bestFriends = 1;
+
+var ageOfSister = 41;
+var ageOfBrother = 45;
+
+var myFirstName = "Cedric";
+var myNewFirstName = "Shawd";
+
+var myLastName = "Smith";
+var myRealLastName = "Bennett";
+
+var middleInitial = "B";
+var alternateMiddleName = "Bernard";
+
+console.log(100 - 40);
+console.log(1 + 1);
+
+console.log(myFirstName + middleInitial + myLastName);
